9754b8a87f4480c69ee51fb287fffe1b2c8aab4a
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-16 00:44:54 +0100

    Add integration tests with Xunit and FluentAssertions (#120)
    * Create GlobalUsings.cs
    
    * Create SolutionName.Tests.Workers.IntegrationTests.csproj
    
    * Create UnitTest1.cs
    
    ---------
    
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

---
+++ Tests/SolutionName.Tests.Workers.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+
+namespace SolutionName.Tests.UnitTests;
+
+/// <summary>
+/// Class UnitTest1.
+/// </summary>
+public class UnitTest1
+{
+    /// <summary>
+    /// Defines the test method Test1.
+    /// </summary>
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var expected = "Hello World!";
+
+        // Act
+
+        // Assert
+        expected.Should().Be("Hello World!");
+    }
+}
